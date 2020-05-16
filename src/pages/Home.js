5cba95349408c30b4f6cb150efba5def88daa978
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-05-16 12:24:14 -0500

    petgram-app-created

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ListOfCategories } from '../components/ListOfCategories'
+import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
+import { Layout } from '../components/Layout'
+
+const HomePage = ({ categoryId }) => {
+  return (
+    <Layout title='Tu aplicación de fotos de mascotas' subtitle='ConPetgram puedes encontrar fotos de animales domésticos muy bonitos'>
+      <ListOfCategories />
+      <ListOfPhotoCards categoryId={categoryId} />
+    </Layout>
+  )
+}
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.categoryId === props.categoryId
+})
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+const path = require('path')
+
+module.exports = {
+  output: {
+    filename: 'app-bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    host: '192.168.1.2',
+    port: 8080,
+    disableHostCheck: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram',
+      description: 'con petgram puedes encontrar fotos de animales domésticos muy facilmente',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('Icons'),
+          ios: true
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-esteban-4qyybw8hb.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
